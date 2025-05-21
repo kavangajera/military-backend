0bcd688bf5b8e83598a1196741fe9b938c080334
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-05-21 19:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ from flask import Flask,jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    load_dotenv()
 
-    @app.route('/')
-    def index():
-        return jsonify({"message": "Welcome to the Military API!"}),200
+app = Flask(__name__)
+CORS(app)
+load_dotenv()
 
-    return app
+@app.route('/')
+def index():
+    return jsonify({"message": "Welcome to the Military API!"}),200
+
+
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(host='0.0.0.0',port=5050,debug=True)
